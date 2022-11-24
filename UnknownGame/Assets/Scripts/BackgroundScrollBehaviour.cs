60f151760527fc6a4f3b21dc9952ffbcd28a8295
hank <10811528@my.uvu.edu>
2022-11-23 21:08:31 -0700

    save

--- UnknownGame/Assets/Scripts/BackgroundScrollBehaviour.cs
+++ UnknownGame/Assets/Scripts/BackgroundScrollBehaviour.cs
@@ -1,4 +1,6 @@
 //started and edited script based on given script on https://tengel403.medium.com/how-create-a-scrolling-background-with-parallax-effect-in-unity-18c070ba149a
+
+using System.Collections;
 using UnityEngine;
 
 public class BackgroundScrollBehaviour : MonoBehaviour
@@ -6,18 +8,32 @@ public class BackgroundScrollBehaviour : MonoBehaviour
     public FloatData scrollSpeed;
     public Vector3Data startPos;
     public Vector3Data cameraPos;
-    private Vector3 backgroundPos;
-    
+    private WaitForFixedUpdate wffu;
+    public BoolData canRun;
+
     public float distance;
     public float modifier;
 
-    void Update()
+    public void StartRepeatUntilFalse()
     {
-        backgroundPos[0] = startPos.value[0]+ cameraPos.value[0];
-        backgroundPos[1] = startPos.value[1];
-        backgroundPos[2] = startPos.value[2];
+        canRun.value = true;
+        StartCoroutine(StartScroll());
+    }
+
+    private IEnumerator StartScroll()
+    {
+        while (canRun.value) 
+        {
+            Scroll();
+            yield return wffu;
+        }
+    }
+    
+    private void Scroll()
+    {
+        startPos.value[0] = cameraPos.value[0];
         
         float newPos = Mathf.Repeat(Time.time * -scrollSpeed.value/modifier, distance);
-        transform.position = backgroundPos + Vector3.right * newPos;
+        transform.position = startPos.value + Vector3.right * newPos;
     }
 }
