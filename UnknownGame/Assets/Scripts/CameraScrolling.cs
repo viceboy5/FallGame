7457c3ee8faa7db8c674c335b1735442f025f889
Hank Weiss <10811528@my.uvu.edu>
2022-11-29 18:23:22 -0700

    setup background scroll via canvas

---
+++ UnknownGame/Assets/Scripts/CameraScrolling.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraScrolling : MonoBehaviour
+{
+    [SerializeField] private RawImage background;
+    [SerializeField] private float x, y;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        background.uvRect = new Rect(background.uvRect.position + new Vector2(x,y) * Time.deltaTime, background.uvRect.size);
+    }
+}
