8813c68a37d35e16a5b9c639d76d99853b1191cf
Hank Weiss <10811528@my.uvu.edu>
2022-11-29 18:33:59 -0700

    canvas scrolls based on speed using update

--- UnknownGame/Assets/Scripts/CameraScrolling.cs
+++ UnknownGame/Assets/Scripts/CameraScrolling.cs
@@ -5,11 +5,12 @@ public class CameraScrolling : MonoBehaviour
 {
     [SerializeField] private RawImage background;
     [SerializeField] private float x, y;
+    public FloatData speed;
     
 
     // Update is called once per frame
     void Update()
     {
-        background.uvRect = new Rect(background.uvRect.position + new Vector2(x,y) * Time.deltaTime, background.uvRect.size);
+        background.uvRect = new Rect(background.uvRect.position + new Vector2(x,y) * (speed.value * Time.deltaTime), background.uvRect.size);
     }
 }
