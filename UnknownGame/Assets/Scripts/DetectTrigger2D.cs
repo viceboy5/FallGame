0a7362dfdc0593f66e239ccae80a139c38fa103e
Hank Weiss <10811528@my.uvu.edu>
2022-12-07 20:15:36 -0700

    changed trigger detection to only find player

--- UnknownGame/Assets/Scripts/DetectTrigger2D.cs
+++ UnknownGame/Assets/Scripts/DetectTrigger2D.cs
@@ -4,10 +4,15 @@ using UnityEngine.Events;
 public class DetectTrigger2D : MonoBehaviour
 {
     public UnityEvent triggerEvent;
+    public GameObject player;
     
-    
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
     void OnTriggerEnter2D(Collider2D other)  // Once the Trigger has been entered record collision in the argument variable "other"
     {
-        triggerEvent.Invoke();
+        if (other.gameObject.name == player.name)
+            triggerEvent.Invoke();
     }
 }
